0b00721da01cf8ba4ffd19e35f096d864e3a51a1
giulianmolinari <102676490+giulianmolinari@users.noreply.github.com>
2022-06-03 10:46:01 -0500

    10:45

---
+++ Laboratorio03/js/biblioteca.js
@@ -0,0 +1,12 @@
+La etiqueta script para poder especificar el tipo de script que se agregara es importante agregarle el type="text/javascript", dentro de esta etiqueta se interpreta el lenguaje de programacion javascript, donde cada linea de codigo de javascript(JS) se cierra con (;) javascript como lenguaje de programacion permite realizar operaciones aritmeticas, funciones como cualquier otro lenguaje de programacion, con la unica diferencia que  va a necesitar un HTML para poder ejecutarse  Para poder mejorar el proceso de aprendizaje de programacion, debemos de tener en cuenta lo siguiente:
+escribir
+variable
+lectura de datos
+operaciones
+estrucutras logicas
+arreglos
+1.-Escritura de datos 
+Permite mostrar informacion en la pagina web, para poder realizar esto utilizamos la sentencia alert o el document.write
+
+Sintaxis alert
+// alert("mensaje");
